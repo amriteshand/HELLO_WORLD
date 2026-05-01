39b8f411784e7ae8ef4c6a7d4bc6b11d35691399
AMRITESH ANAND <anandamritesh2006@gmail.com>
2026-05-01 14:59:17 +0530

    Add UC2 file

---
+++ uc2_displayHELLO.java
@@ -0,0 +1,18 @@
+/**
+ * UC2: Display "Hello" in the console
+ * This program demonstrates a basic Java application
+ * with a properly structured main method.
+ */
+public class uc2_displayHELLO {
+
+    /**
+     * Entry point of the program
+     * @param args command-line arguments (not used here)
+     */
+    public static void main(String[] args) {
+
+        // Display message to console
+        System.out.println("Hello");
+
+    }
+}
