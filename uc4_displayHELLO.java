2b66ae27b6ee9c54764bc178beeb6ed991ac5ce8
AMRITESH ANAND <anandamritesh2006@gmail.com>
2026-05-01 15:21:30 +0530

    uc4 submitted by AMRITESH anand

---
+++ uc4_displayHELLO.java
@@ -0,0 +1,19 @@
+/**
+ * UC4: Display Hello message with multiple lines and formatting
+ * This program demonstrates structured output and clean coding practices.
+ */
+public class uc4_displayHELLO {
+
+    /**
+     * Entry point of the program
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display multiple lines of output
+        System.out.println("Welcome to Java Programming");
+        System.out.println("Hello");
+        System.out.println("This is UC4 Assignment");
+
+    }
+}
