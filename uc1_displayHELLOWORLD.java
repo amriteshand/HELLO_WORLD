41eb7a075a41ab4d85dde39f1caec960d537b0ef
AMRITESH ANAND <anandamritesh2006@gmail.com>
2026-05-01 14:21:35 +0530

    UC1 SUBMITTED BY AMRITESH ANAND

---
+++ uc1_displayHELLOWORLD.java
@@ -0,0 +1,9 @@
+public class uc1_displayHELLOWORLD {
+
+    // Entry point of the program
+    public static void main(String[] args) {
+        // Print message to console
+        System.out.println("Hello World");
+    }
+
+}
